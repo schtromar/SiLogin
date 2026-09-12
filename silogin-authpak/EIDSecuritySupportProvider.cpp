1e3650106fdb6f4e2a96c37f4f8cec944ec62893
schtromar <luka.macek@siol.net>
2026-09-12 09:25:48 +0200

    Sorted out license headers

--- silogin-authpak/EIDSecuritySupportProvider.cpp
+++ silogin-authpak/EIDSecuritySupportProvider.cpp
@@ -1,3 +1,28 @@
+﻿/*
+    EID Authentication - Smart card authentication for Windows
+    Copyright (C) 2009 Vincent Le Toux
+    Copyright (C) 2026 Contributors
+    Copyright (C) 2026 Luka Maček
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+	This file was originally part of the EIDAuthentication package
+    and has been modified for use in the silogin-authpak project.
+*/
+
 #include <ntstatus.h>
 #define WIN32_NO_STATUS
 #include <Windows.h>
